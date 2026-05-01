875f2f03b9499bdba0446c629aa773a0954c65cc
alaa-eldsoky <alaaeldsoky450@gmail.com>
2026-05-01 23:47:15 +0300

    Add files via upload

---
+++ FlightReservationSystem/Booking.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace FlightReservationSystem
+{
+    class Booking
+    {
+        public string FlightNumber { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public string TravelClass { get; set; }
+        public Passenger Passenger { get; set; }
+
+        public Booking(string flight, DateTime date, string tClass, Passenger p)
+        {
+            FlightNumber = flight;
+            DepartureDate = date;
+            TravelClass = tClass;
+            Passenger = p;
+        }
+    }
+}
